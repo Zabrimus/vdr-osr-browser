036e6cfe6c98c06a32ccf7f8cde3b08d1d83bb61
Zabrimus <github@hannebauer.org>
2020-03-16 17:06:42 +0100

    shell script to create test streams and to play with ffmpeg parameters (very very rudimentary)

---
+++ tools/create_ts_files.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# ffmpeg binary
+FFMPEG=/usr/bin/ffmpeg
+
+# input video file, adapt accordingly
+VIDEO_IN=somenight.mp4
+
+# command used in browser (unoptimized, test-only, possibly broken)
+$FFMPEG -y -r 25 -i $VIDEO_IN -f mpegts -q:v 10 -an -vcodec mpeg4 test1.ts
+
+
